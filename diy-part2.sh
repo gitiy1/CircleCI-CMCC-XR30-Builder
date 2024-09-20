09379aa33be7422deec83e865e3c7f8131d51fba
Git IY! <91715524+gitiy1@users.noreply.github.com>
2024-09-20 18:58:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,7 +10,8 @@
 
 function config_del(){
     yes="CONFIG_$1=y"
-    no="# CONFIG_$1 is not set"
+    # no="# CONFIG_$1 is not set"
+    no="CONFIG_$1=n"
 
     sed -i "s/$yes/$no/" .config
 }
@@ -174,6 +175,12 @@ config_package_del luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Server
 config_package_del luci-app-passwall_INCLUDE_Trojan_Plus
 config_package_del luci-app-passwall_INCLUDE_Simple_Obfs
 config_package_del luci-app-passwall_INCLUDE_tuic_client
+config_package_del shadowsocks-libev-config
+config_package_del shadowsocks-libev-ss-local
+config_package_del shadowsocks-libev-ss-redir
+config_package_del shadowsocks-libev-ss-server
+config_package_del shadowsocksr-libev-ssr-local
+config_package_del shadowsocksr-libev-ssr-redir
 
 
 ## 定时任务。重启、关机、重启网络、释放内存、系统清理、网络共享、关闭网络、自动检测断网重连、MWAN3负载均衡检测重连、自定义脚本等10多个功能
